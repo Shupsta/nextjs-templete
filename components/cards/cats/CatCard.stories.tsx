faaa379e7bb755b22c41c66753ec3f1b57c9c479
Nick <shupsta2010@gmail.com>
2022-05-03 19:02:38 -0700

    feat: catCard cant get image to work in Storybook

--- components/cards/cats/CatCard.stories.tsx
+++ components/cards/cats/CatCard.stories.tsx
@@ -3,7 +3,7 @@ import CatCard, { ICatCard } from './CatCard';
 import { mockCatCardProps } from './CatCard.mocks';
 
 export default {
-  title: 'templates/CatCard',
+  title: 'cards/CatCard',
   component: CatCard,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.pravatar.cc'],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 module.exports = nextConfig;
